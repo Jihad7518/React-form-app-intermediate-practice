eead1a176256b9fe51492386122b49831bc815ce
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-16 17:28:36 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+
+  const [formData, setFormData] = useState({
+    firstName:"", lastName:"", email:"", country:"India",
+    streetAddress:"", city:"", state:"", postalCode:"",
+    comments:false, candidates:false, offers:false, pushNotifications:""
+  })
+
+  function changeHandler(event) {
+    const {name, value, checked, type} = event.target;
+    setFormData( (prev) => ({...prev, [name]:type === "checkbox" ? checked: value}) );
+  }
+  
