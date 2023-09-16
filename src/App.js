9cdc7784a87d0a16fa2fa76d36ad1a60b83500bb
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-16 17:31:35 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -208,3 +208,32 @@ function App() {
         onChange={changeHandler}
       />
           
+      <label htmlFor="pushEmail">Same as email</label>
+
+      <br/>
+      <input
+        type="radio"
+        id="pushNothing"
+        name="pushNotifications"
+        value="No Push Notifications"
+        onChange={changeHandler}
+      />
+
+      <label htmlFor="pushNothing">No Push Notifications</label>
+
+    </fieldset>
+
+
+
+   <button
+   className="bg-blue-500 text-white font-bold rounded py-2 px-4"
+   >Save</button>
+
+
+
+
+
+
+   </form>
+
+   </div>
